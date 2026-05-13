923a85206b786e6071328cc6e2bccbb826c5f566
i2rlearning <162467907+i2rlearning@users.noreply.github.com>
2026-05-13 17:17:02 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -218,7 +218,7 @@ function renderMyNotes(notes) {
       <td>${preview}</td>
       <td>${formatDate(note.updatedAt)}</td>
       <td>
-        ${note.pageUrl ? `<a href="${note.pageUrl} style="text-decoration: none; font-size: 24px; cursor: pointer; display: flex; align-items: center; justify-content: center;">📂</a>` : ""}
+        ${note.pageUrl ? `<a href="${note.pageUrl} style="text-decoration: none; font-size: 24px; cursor: pointer; display: flex; align-items: center; justify-content: center;">🚪</a>` : ""}
       </td>
       <td>
         <a href="#" class="delete-note" data-id="${note.pageKey}" title="Delete Note" style="text-decoration: none; color: #ff4d4d; font-size: 24px; cursor: pointer; display: flex; align-items: center; justify-content: center;">🗑
