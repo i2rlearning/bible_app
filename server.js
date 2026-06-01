394cfa86c9bf8c3e99943d1e8a16ec21a50d240d
i2rlearning <162467907+i2rlearning@users.noreply.github.com>
2026-06-01 18:10:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -468,7 +468,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Bible app running on port ${PORT}`);
 });
