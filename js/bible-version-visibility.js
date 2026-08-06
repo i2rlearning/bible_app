60d322720acec774c6270fe2aadd5596a01add72
i2rlearning <162467907+i2rlearning@users.noreply.github.com>
2026-08-06 19:02:20 +0300

    Update bible-version-visibility.js

--- js/bible-version-visibility.js
+++ js/bible-version-visibility.js
@@ -1,4 +1,8 @@
 window.HiddenBibleVersions = [
+  "685d1470fe4d5c3b-01",
+  "179568874c45066f-01",
+  "55ec700d9e0d77ea-01",
+  "2f0fd81d7b85b923-01",
   "9879dbb7cfe39e4d-02",
   "9879dbb7cfe39e4d-03",
   "9879dbb7cfe39e4d-04"
